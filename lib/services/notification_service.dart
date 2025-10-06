dbb579f873204de05b0b2b801606dd5422848b18
AndresPab <andysapsanty@gmail.com>
2025-10-06 10:14:25 -0500

    eliminación de archivos notification_service y sus versiones web y mobile, y actualización del modulo de recordatorios, modificaciones de pantallas

--- lib/services/notification_service.dart
+++
@@ -1,6 +0,0 @@
-// lib/services/notification_service.dart
-
-// Este archivo **reexporta** la implementación correcta
-// según si estamos en web (dart.library.html) o no.
-export 'notification_service_mobile.dart'
-    if (dart.library.html) 'notification_service_web.dart';
